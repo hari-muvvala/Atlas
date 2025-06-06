97665a88ec824d5a585580fea89f08403f6fcd51
hari-muvvala <muvvalaharigopal@gmail.com>
2025-06-06 18:23:34 +0530

    Added Tasks 19 and 20.java

--- src/Task017.java
+++ src/Task017.java
@@ -1,7 +1,10 @@
 public class Task017 {
     public static void main(String[] args) {
         Person myObj = new Person();
-        myObj.name = "John"; //Error: name has private access
-        System.out.println(myObj.name); //Error: name is not accessible
+        //myObj = new Person();
+        myObj.setName("John"); //Initial Error: name has private access; Next changed to setName; set value using setter
+        //System.out.println(myObj.name);
+        System.out.println(myObj.getName()); //Error: name is not accessible; after change getName is accessible; Get value using Getter
+    //After the error used setter and getter
     }
 }
